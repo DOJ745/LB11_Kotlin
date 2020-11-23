8d6f649f155bfe44ceb9d6ef97d807376c6e52f2
DOJ745 <andreyfa12@mail.ru>
2020-11-23 21:30:15 +0300

    Task 2 complete

--- src/LB11.kt
+++ src/LB11.kt
@@ -1,7 +1,11 @@
 // Task 1
-fun main(args: Array<String>){
 
-    val palindrome = "Dot saw I was Tod"
+        // D
+const val RANDOM_CONST:Int = 42
+
+fun main(args: Array<String>) {
+
+    /*val palindrome = "Dot saw I was Tod"
     val len = palindrome.length
     val tempCharArray = CharArray(len)
     val charArray = CharArray(len)
@@ -13,15 +17,38 @@ fun main(args: Array<String>){
     }
     val reversePalindrome = String(charArray)
     println(reversePalindrome)
-}
+*/
 
     // Task 2
 
-// A
-// B
-// C
-// D
-// E
+        // A
+    val withoutTypeInt: Int = 34;
+    val withoutTypeDouble: Double = 3.14;
+    val withoutTypeStr: String = "1234";
+
+    var wthTypeInt = 45;
+    var wthTypeStr = "1234";
+    var wthTypeDouble = 3.19;
+
+        // B
+
+    val byteType: Byte = 44;
+    val byteToInt = byteType.toInt();
+    val intToStr: String = byteToInt.toString();
+
+        // C
+
+    println("BYTE -  + $byteType");
+    println("BYTE TO INT - $byteToInt");
+    println("INT TO STRING - $intToStr");
+
+        // D - watch in top of code
+
+        // E
+
+    var nullInt: Int? = null;
+    var str = readLine();
+    println(str);
 
     // Task 3
 
@@ -42,3 +69,5 @@ fun main(args: Array<String>){
 // A
 // B
 // C
+
+}
