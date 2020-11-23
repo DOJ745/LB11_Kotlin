81c92a08b7d738e688cc15bb77c78161f5a2b0d1
DOJ745 <andreyfa12@mail.ru>
2020-11-23 22:32:27 +0300

    Task 3 b) complete

--- src/LB11.kt
+++ src/LB11.kt
@@ -46,28 +46,62 @@ fun main(args: Array<String>) {
 
         // E
 
-    var nullInt: Int? = null;
+    /*var nullInt: Int? = null;
     var str = readLine();
-    println(str);
+    println(str);*/
 
     // Task 3
 
-// A
-// B
-// C
-// D
-// E
-// F
+        // A
+    fun sumOfDouble(vararg values: Double): Double {
+        var sum: Double = 0.0
+            for (n in values)
+            sum += n
+            return sum
+    }
+
+    println("Sum of double =  " + sumOfDouble(3.14, 5.55, 1.99))
+
+        // B
+
+    println("Input email: ")
+    val emailIn: String = readLine()!!
+    println("Input password: ")
+    val passwordIn: String = readLine()!!
+
+    fun isValid(email: String , password: String): Boolean {
+        val emailReg = Regex(pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
+        val passwordReg = Regex(pattern = "\\s")
+
+        val matchedEmail = emailReg.containsMatchIn(email)
+        val matchedPassword = !passwordReg.containsMatchIn(password)
+        fun notNull(str: String): Boolean = str.isNotEmpty()
+
+        val passwLength: Boolean = !(password.length < 6 || password.length > 12)
+
+        return ( notNull(email) && notNull(password) ) && ( matchedEmail && matchedPassword )
+                && passwLength
+    }
+
+    if (isValid(emailIn, passwordIn))
+        println("Success registration!")
+    else
+        println("Failed registration!")
+
+        // C
+        // D
+        // E
+        // F
 
     // Task 4
 
-// A
-// B
+        // A
+        // B
 
     // Task 5
 
-// A
-// B
-// C
+        // A
+        // B
+        // C
 
 }
